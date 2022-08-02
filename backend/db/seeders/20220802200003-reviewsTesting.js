35cd828d54edb681ef25745df1a75e1854169015
argon <argonglach@gmail.com>
2022-08-02 19:32:27 -0400

    fixed typo in reviews seeder

--- backend/db/seeders/20220802200003-reviewsTesting.js
+++ backend/db/seeders/20220802200003-reviewsTesting.js
@@ -7,25 +7,25 @@ module.exports = {
       review: 'this is a review for spot 1',
       stars: 4,
       userId: 1,
-      spotid: 1,
+      spotId: 1,
     },
     {
       review: 'this is another review for spot 1',
       stars: 2,
       userId: 2,
-      spotid: 1,
+      spotId: 1,
     },
     {
       review: 'this is another review for spot 2',
       stars: 4,
       userId: 1,
-      spotid: 2,
+      spotId: 2,
     },
     {
       review: 'this is another review for spot 2',
       stars: 5,
       userId: 2,
-      spotid: 2,
+      spotId: 2,
     },
 
 
