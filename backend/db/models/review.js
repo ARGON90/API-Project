b0827ff402d5a31e964e251cf96dc7a3bc2beb46
argon <argonglach@gmail.com>
2022-08-02 19:30:41 -0400

    fixed error in review migration

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       Review.belongsTo(models.User, {foreignKey: 'userId' });
-      Review.belongsTo(models.Spot, { foreignKey: 'spotid'})
+      Review.belongsTo(models.Spot, { foreignKey: 'spotId'})
     }
   }
   Review.init({
