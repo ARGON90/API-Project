f460cfe6ef8cd2ea5e03f767f1b849370fa04b44
argon <argonglach@gmail.com>
2022-08-02 15:44:58 -0400

    spots/current route additions

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
 
-const { Spot } = require('../../db/models')
+const { Spot, User } = require('../../db/models')
 
 router.get('/', async (req, res) => {
     const allSpots = await Spot.findAll();
     res.json(allSpots)
     // previewImg: images
-    // stars from reviews
+    // avgstars from reviews
 })
+
+router.get('/current', requireAuth, async (req, res) => {
+    const { user } = req;
+
+    if (user) {
+        const currentSpots = await Spot.findAll ({
+            where: {ownerId: user.id}
+
+        })
+        return res.json({currentSpots});
+
+    } else return res.json({});
+    //add avg rating,
+    //add preview img
+}
+);
 
 module.exports = router;
