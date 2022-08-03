6afa0e4c095918f724e541e9c6481b52349784a0
argon <argonglach@gmail.com>
2022-08-03 12:22:59 -0400

    edited :spotId to be lazy

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -4,6 +4,8 @@ const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth')
 
 const { Spot, User, Review, Image, sequelize } = require('../../db/models');
 const app = require('../../app');
+const { handleValidationErrors } = require('../../utils/validation');
+const { check } = require('express-validator');
 
 router.get('/', async (req, res) => {
     const allSpots = await Spot.findAll({
@@ -56,15 +58,53 @@ router.get('/current', requireAuth, async (req, res) => {
 
 });
 
+//EAGER LOAD OF /SPOTID
+// router.get('/:spotId', async (req, res) => {
+//     const { spotId } = req.params
+//     const spotInfo = await Spot.findAll({
+//         where: { id: spotId },
+//         include: [
+//             {
+//                 model: Review,
+//                 attributes: []
+//             },
+//             {
+//                 model: Image,
+//                 attributes: ['id', 'url']
+//             },
+//             {
+//                 model: User, as: "Owner",
+//                 attributes: ['id', 'firstName', 'lastName']
+//             },
+//         ],
+//         // attributes: {
+//         //     include: [
+//         //         [sequelize.fn("AVG", sequelize.col("stars")), "avgRating"],
+//         //     ],
+//         // },
+
+//     })
+//     if (spotInfo[0].dataValues.id) {
+//         res.json(spotInfo)
+//         //STILLNEEDS imageable attribute - what even is that?
+//         //STILLNEEDS image listing fix
+//     } else {
+//         res.status(404)
+//         res.json({
+//             message: "Spot couldn't be found",
+//             statusCode: 404
+//         })
+//     }
+
+// })
+
+
 router.get('/:spotId', async (req, res) => {
     const { spotId } = req.params
     const spotInfo = await Spot.findAll({
+        group: ['spotId'],
         where: { id: spotId },
         include: [
-            {
-                model: Review,
-                attributes: []
-            },
             {
                 model: Image,
                 attributes: ['id', 'url']
@@ -74,16 +114,29 @@ router.get('/:spotId', async (req, res) => {
                 attributes: ['id', 'firstName', 'lastName']
             },
         ],
+    })
+
+    const avgRating = await Spot.findOne({
+        where: { id: spotId },
+        attributes: [],
+        include: [{
+            model: Review,
+            attributes: []
+        },
+        ],
         attributes: {
-            include: [
+        include: [
                 [sequelize.fn("AVG", sequelize.col("stars")), "avgRating"],
             ],
         },
-
     })
+    const avgStarRating = avgRating.dataValues.avgRating
+
+
     if (spotInfo[0].dataValues.id) {
-        res.json(spotInfo)
+        res.json({spotInfo, avgStarRating})
         //STILLNEEDS imageable attribute - what even is that?
+        //STILLNEEDS image listing fix
     } else {
         res.status(404)
         res.json({
@@ -94,6 +147,21 @@ router.get('/:spotId', async (req, res) => {
 
 })
 //Question: why is the attributes for avgstar rating limiting the number of images I can display?
+
+
+const validateCreate = [
+    check('address')
+        .exists({ checkFalsy: true })
+        .withMessage('Street address is required'),
+    check('username')
+        .exists({ checkFalsy: true })
+        .withMessage('Username is required'),
+    check('name')
+        .exists({ checkFalsy: true })
+        .isLength({ max: 50 })
+        .withMessage('Name must be less than 50 characters'),
+    handleValidationErrors
+];
 
 router.post('/', requireAuth, async (req, res) => {
     const userId = req.user.id
@@ -115,11 +183,13 @@ router.post('/', requireAuth, async (req, res) => {
     })
 
     const spotResult = await Spot.findAll({
-        where: {address: address}
+        where: { address: address }
     })
+
     res.status(201)
     return res.json(spotResult)
 })
 //question: is using the address cheating for findAll...?
+
 
 module.exports = router;
