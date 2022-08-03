81718a9a0f409ced1cf4dbb2ff3854a7cce83690
argon <argonglach@gmail.com>
2022-08-03 14:51:41 -0400

    create a spot route done

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -58,46 +58,6 @@ router.get('/current', requireAuth, async (req, res) => {
 
 });
 
-//EAGER LOAD OF /SPOTID
-// router.get('/:spotId', async (req, res) => {
-//     const { spotId } = req.params
-//     const spotInfo = await Spot.findAll({
-//         where: { id: spotId },
-//         include: [
-//             {
-//                 model: Review,
-//                 attributes: []
-//             },
-//             {
-//                 model: Image,
-//                 attributes: ['id', 'url']
-//             },
-//             {
-//                 model: User, as: "Owner",
-//                 attributes: ['id', 'firstName', 'lastName']
-//             },
-//         ],
-//         // attributes: {
-//         //     include: [
-//         //         [sequelize.fn("AVG", sequelize.col("stars")), "avgRating"],
-//         //     ],
-//         // },
-
-//     })
-//     if (spotInfo[0].dataValues.id) {
-//         res.json(spotInfo)
-//         //STILLNEEDS imageable attribute - what even is that?
-//         //STILLNEEDS image listing fix
-//     } else {
-//         res.status(404)
-//         res.json({
-//             message: "Spot couldn't be found",
-//             statusCode: 404
-//         })
-//     }
-
-// })
-
 
 router.get('/:spotId', async (req, res) => {
     const { spotId } = req.params
@@ -106,13 +66,23 @@ router.get('/:spotId', async (req, res) => {
         include: [
             {
                 model: Image,
-                attributes: ['id', 'url']
+                attributes: ['id', 'url'],
+                attributes: {
+                    include: [
+                        [sequelize.literal("Spot.id"), "imageableId"],
+                    ],
+                }
             },
             {
                 model: User, as: "Owner",
                 attributes: ['id', 'firstName', 'lastName']
             },
         ],
+        // attributes: {
+        //     include: [
+        //         [sequelize.literal("Spot.id"), "imageableId"],
+        //     ],
+        // }
     })
 
     const avgRating = await Spot.findOne({
@@ -123,7 +93,7 @@ router.get('/:spotId', async (req, res) => {
         },
         ],
         attributes: {
-        include: [
+            include: [
                 [sequelize.fn("AVG", sequelize.col("Reviews.stars")), "avgRating"],
             ],
         },
@@ -133,9 +103,9 @@ router.get('/:spotId', async (req, res) => {
 
 
     if (spotInfo[0].dataValues.id) {
-        res.json({spotInfo, avgStarRating})
-        //STILLNEEDS imageable attribute - what even is that?
-        //STILLNEEDS image listing fix
+        res.json({ spotInfo, avgStarRating })
+        //STILLNEEDS imageable attribute how to get it formatted correctly
+        //STILLNEEDS heroku decimal fix
     } else {
         res.status(404)
         res.json({
@@ -148,25 +118,45 @@ router.get('/:spotId', async (req, res) => {
 //Question: why is the attributes for avgstar rating limiting the number of images I can display?
 
 
-const validateCreate = [
-    check('address')
-        .exists({ checkFalsy: true })
-        .withMessage('Street address is required'),
-    check('state')
-        .exists({ checkFalsy: true })
-        .withMessage('State is required'),
-    check('name')
-        .exists({ checkFalsy: true })
-        .isLength({ max: 50 })
-        .withMessage('Name must be less than 50 characters'),
-    handleValidationErrors
-];
+// const validateCreation = [
+//     check('address')
+//         .exists({ checkFalsy: true })
+//         .withMessage('Street address is required'),
+//     check('state')
+//         .exists({ checkFalsy: true })
+//         .withMessage('State is required'),
+//     check('name')
+//         .exists({ checkFalsy: true })
+//         .isLength({ max: 50 })
+//         .withMessage('Name must be less than 50 characters'),
+//     handleValidationErrors
+// ];
 
-router.post('/', requireAuth, validateCreate, async (req, res) => {
+router.post('/', requireAuth, async (req, res) => {
     const userId = req.user.id
-
+    let errors = {}
     const { address, city, state, country, lat,
         lng, name, description, price } = req.body;
+
+    if (!address) errors.address =  "Street address is required"
+    if (!city) errors.city =  "City is required"
+    if (!state) errors.state =  "State is required"
+    if (!country) errors.country =  "Country address is required"
+    if (typeof lat != 'number') errors.lat =  "Latitude is not valid"
+    if (typeof lng != 'number') errors.lng =  "Longitude is not valid"
+    if (!name) errors.name =  "Name is required"
+    if (name.length >= 50) errors.name =  "Name must be less than 50 characters"
+    if (!description) errors.description =  "Description is required"
+    if (!price) errors.price =  "Price is required"
+
+    if (Object.keys(errors).length != 0) {
+        res.status(400)
+        res.json({
+            message: "Validation Error",
+            statusCode: 400,
+            errors
+        })
+    }
 
     const newSpot = await Spot.create({
         ownerId: userId,
@@ -189,6 +179,7 @@ router.post('/', requireAuth, validateCreate, async (req, res) => {
     return res.json(spotResult)
 })
 //question: is using the address cheating for findAll...?
+//are there uniqueness constraints on any of these
 
 
 module.exports = router;
