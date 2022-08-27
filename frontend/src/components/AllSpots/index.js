690a94696bf848bfa0f727b72657d024d5391a57
argon <argonglach@gmail.com>
2022-08-27 12:19:33 -0400

    added styling to all spots

--- frontend/src/components/AllSpots/index.js
+++ frontend/src/components/AllSpots/index.js
@@ -30,8 +30,6 @@ const SpotsList = () => {
         } else {
             return (
                 <img src={spot.previewImage} alt='Main Image' className='
-            flex-shrink-0
-            overflow-hidden
             width-card-image
             height-card-image
             border-radius-12'/>
@@ -43,47 +41,80 @@ const SpotsList = () => {
     if (!spotsList) return <div>Loading All Spots...</div>
     return (
         <>
-            {/* ALLCARDS CONTAINER */}
-            <div className='flex-box justify-content-between  flex-wrap-wrap'>
-                {spotsList.map((spot) => (
+            {/* ALL CARDS CONTAINER */}
+            <div className='
+            flex-box
+            justify-content-between
+            flex-wrap-wrap
+            column-gap-15
+            row-gap-15'>
 
-                    //INDIVIDUAL CARD DIV
-                    <div className=' font-family
-                        overflow-hidden
+                {spotsList.map((spot) => (
+                    <NavLink key={spot.id} to={`/spots/${spot.id}`} className=''>
+
+                        <div className=' font-family
+                        max-width-400
                         flex-column
                         width-card
-                        height-card
-                        padding-card'>
-                        <NavLink key={spot.id} to={`/spots/${spot.id}`}>
+                        height-card'>
+
                             {/* IMG CONTAINER DIV */}
                             <div className='
-                                    flex
-                                    justify-content-center
-                                    align-items-center
                                     height-card-image-container
-                                    width-card-image-container'>
+                                    width-card-image-container
+                                    margin-card-image-container'>
                                 {imageCheck(spot)}
                             </div>
 
-                            {/* TEXT DIV */}
-                            <div>
-                                <p className='font-black bold'>{spot.city}, {spot.state}</p>
-                                <p>
-                                    <svg
-                                        viewBox='0 0 32 32'
-                                    >
-                                        <path
-                                            d='M15.094 1.579l-4.124 8.885-9.86 1.27a1 1 0 0 0-.542 1.736l7.293 6.565-1.965 9.852a1 1 0 0 0 1.483 1.061L16 25.951l8.625 4.997a1 1 0 0 0 1.482-1.06l-1.965-9.853 7.293-6.565a1 1 0 0 0-.541-1.735l-9.86-1.271-4.127-8.885a1 1 0 0 0-1.814 0z'
-                                            fillRule='evenodd'
-                                        ></path>
-                                    </svg>
-                                    {spot.avgRating}
-                                </p>
-                                <p className='font-grey margin-remove'>${spot.price}/night</p>
+                            {/* TEXT CONTAINER */}
+                            <div className='
+                            flex-column'>
+
+                                <div>
+                                    {/* LOCATION / STAR CONTAINER  */}
+                                    <div className='
+                                font-black
+                                flex-row
+                                justify-content-between'>
+                                        <div className='
+                                        bold'>
+                                            {spot.city}, {spot.state}
+                                        </div>
+
+                                        <div>
+                                            <svg viewBox='0 0 32 32'>
+                                                <path
+                                                    d='M15.094 1.579l-4.124 8.885-9.86 1.27a1 1 0 0 0-.542 1.736l7.293 6.565-1.965 9.852a1 1 0 0 0 1.483 1.061L16 25.951l8.625 4.997a1 1 0 0 0 1.482-1.06l-1.965-9.853 7.293-6.565a1 1 0 0 0-.541-1.735l-9.86-1.271-4.127-8.885a1 1 0 0 0-1.814 0z'
+                                                    fillRule='evenodd'
+                                                ></path>
+                                            </svg>
+                                            {spot.avgRating}
+                                        </div>
+
+                                    </div>
+
+                                    <div className='
+                                    margin-top-6
+                                    flex-row'>
+                                        <div className='
+                                        font-black
+                                        bold'>
+                                            ${spot.price}
+                                        </div>
+                                        <div className='
+                                        font-grey'>
+                                            /night
+                                        </div>
+                                    </div>
+
+                                </div>
+
                             </div>
-                        </NavLink>
-                    </div>
+
+                        </div>
+                    </NavLink>
                 ))}
+
             </div>
         </>
     );
