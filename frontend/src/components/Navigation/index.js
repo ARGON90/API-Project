7de4c061c3342578e86a5595cecfdd477e5b4ecf
argon <argonglach@gmail.com>
2022-08-27 19:11:16 -0400

    profile button display same on login/logout

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -76,8 +76,31 @@ function Navigation({ isLoaded }) {
       <>
         {/* <ProfileButton user={sessionUser} /> */}
         <div class="drop-menu">
-          <button onClick={() => showHide()} class="dropbtn">LOGO</button>
+          <button onClick={() => showHide()} class="dropbtn">
 
+          <div className='
+            pointer-events-none
+            flex-row
+            justify-content-around
+            align-items-center'>
+
+              <div className=''>
+                <img src={hamburger} alt='hamburger' className='hamburger
+                padding-top-5
+                padding-right-5'>
+                </img>
+              </div>
+
+              <div className='
+              padding-right-5'>
+                <svg className='profile-svg ' viewBox='0 0 32 32'>
+                  <path
+                    d='m16 .7c-8.437 0-15.3 6.863-15.3 15.3s6.863 15.3 15.3 15.3 15.3-6.863 15.3-15.3-6.863-15.3-15.3-15.3zm0 28c-4.021 0-7.605-1.884-9.933-4.81a12.425 12.425 0 0 1 6.451-4.4 6.507 6.507 0 0 1 -3.018-5.49c0-3.584 2.916-6.5 6.5-6.5s6.5 2.916 6.5 6.5a6.513 6.513 0 0 1 -3.019 5.491 12.42 12.42 0 0 1 6.452 4.4c-2.328 2.925-5.912 4.809-9.933 4.809z'>
+                  </path>
+                </svg>
+              </div>
+            </div>
+          </button>
 
           <div id="drop-id" class="drop-content
           flex-column
@@ -119,18 +142,16 @@ function Navigation({ isLoaded }) {
             justify-content-around
             align-items-center'>
 
-              <div className='pointer-events-none'>
+              <div className=''>
                 <img src={hamburger} alt='hamburger' className='hamburger
-                pointer-events-none
                 padding-top-5
                 padding-right-5'>
                 </img>
               </div>
 
               <div className='
-              pointer-events-none
               padding-right-5'>
-                <svg className='profile-svg pointer-events-none' viewBox='0 0 32 32'>
+                <svg className='profile-svg ' viewBox='0 0 32 32'>
                   <path
                     d='m16 .7c-8.437 0-15.3 6.863-15.3 15.3s6.863 15.3 15.3 15.3 15.3-6.863 15.3-15.3-6.863-15.3-15.3-15.3zm0 28c-4.021 0-7.605-1.884-9.933-4.81a12.425 12.425 0 0 1 6.451-4.4 6.507 6.507 0 0 1 -3.018-5.49c0-3.584 2.916-6.5 6.5-6.5s6.5 2.916 6.5 6.5a6.513 6.513 0 0 1 -3.019 5.491 12.42 12.42 0 0 1 6.452 4.4c-2.328 2.925-5.912 4.809-9.933 4.809z'>
                   </path>
