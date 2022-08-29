711346ee7ab6ec9c233e01930b700a72987d1101
argon <argonglach@gmail.com>
2022-08-29 09:17:34 -0400

    updated edit page

--- frontend/src/components/EditSpot/index.js
+++ frontend/src/components/EditSpot/index.js
@@ -9,7 +9,7 @@ import { editSpot } from '../../store/spotsReducer';
 
 function EditSpot() {
     const dispatch = useDispatch();
-    const {spotId} = useParams();
+    const { spotId } = useParams();
     const history = useHistory();
     const [address, setAddress] = useState('');
     const [city, setCity] = useState('');
@@ -36,115 +36,218 @@ function EditSpot() {
         e.preventDefault();
 
         const spotInfo = {
-          address,
-          city,
-          state,
-          country,
-          lat,
-          lng,
-          name,
-          description,
-          price
+            address,
+            city,
+            state,
+            country,
+            lat,
+            lng,
+            name,
+            description,
+            price
         };
 
         console.log('INSIDE CREATE FORM SUBMIT')
         console.log('PAYLOAD', spotInfo)
 
         let editedSpot;
-          editedSpot = await dispatch(editSpot(spotId ,spotInfo));
+        editedSpot = await dispatch(editSpot(spotId, spotInfo));
         if (editedSpot) {
             console.log(editedSpot)
-          history.push(`/spots/${editedSpot.id}`);
+            history.push(`/spots/${editedSpot.id}`);
         }
-      };
+    };
 
 
-      return (
-        <section className='font-family'>
-            <div className='flex-box flex-direction-column'>
-                    <form onSubmit={handleSubmit}>
-                    <div>
-                        <h1>Edit Spot {spotId}</h1>
+    return (
+        <div className='
+        flex-box
+        justify-content-center'>
+
+            {/* // PAGE DIV */}
+            <div className='font-family
+        flex-column
+        width-90'>
+
+                <div className='
+                flex-column
+                align-items-center'>
+
+                    <div className='
+                '>
+                        <h1>Edit Your Spot's Details</h1>
                     </div>
-                    <div>
-                        Address
-                        <input
-                            type="text"
-                            placeholder="Address"
-                            required
-                            value={address}
-                            onChange={updateAddress} />
+
+                    {/* FORM DIV */}
+                    <div className='
+                form-container'>
+
+                        <form onSubmit={handleSubmit} className='
+                    form'>
+
+
+                            <div className='
+                        width-65
+                        flex-column
+                        align-items-center'>
+                                <div
+                                    className='
+                            grey-border
+                            width-90
+                            margin-bottom-30
+                            font-grey
+                            '>
+                                    <div>
+                                        <div>
+                                            <div>
+                                                <div className='padding-left-5' >Address</div>
+                                                <div className='
+                                border-bottom'>
+                                                    <div className='padding-left-5'>
+                                                        <input
+                                                            type="text"
+                                                            placeholder="Address"
+
+                                                            value={address}
+                                                            onChange={updateAddress}
+                                                            className='inputs' />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div>
+                                            <div className='padding-left-5'>City</div>
+                                            <div className='
+                                border-bottom'>
+                                                <div className='padding-left-5'>
+                                                    <input
+                                                        type="text"
+                                                        placeholder="City"
+
+                                                        value={city}
+                                                        onChange={updateCity}
+                                                        className='inputs' />
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div className='padding-left-5' >City</div>
+                                            <div className='
+                                border-bottom'>
+                                                <div className='padding-left-5'>
+                                                    <input
+                                                        type="text"
+                                                        placeholder="State"
+
+                                                        value={state}
+                                                        onChange={updateState}
+                                                        className='inputs' />
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div className='padding-left-5' >Country</div>
+                                            <div className='
+                                border-bottom'>
+                                                <div className='padding-left-5'>
+                                                    <input
+                                                        type="text"
+                                                        placeholder="Country"
+                                                        value={country}
+                                                        onChange={updateCountry}
+                                                        className='inputs' />
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div className='padding-left-5' >Latitude</div>
+                                            <div className='
+                                border-bottom'>
+                                                <div className='padding-left-5'>
+                                                    <input
+                                                        type="number"
+                                                        placeholder="Latitude (numbers only!)"
+                                                        value={lat}
+                                                        onChange={updateLat}
+                                                        className='inputs' />
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div className='padding-left-5' >Longitude</div>
+                                            <div className='
+                                border-bottom'>
+                                                <div className='padding-left-5'>
+                                                    <input
+                                                        type="number"
+                                                        placeholder="Longitude (numbers only!)"
+                                                        value={lng}
+                                                        onChange={updateLng}
+                                                        className='inputs' />
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div className='padding-left-5' >Name</div>
+                                            <div className='
+                                border-bottom'>
+                                                <div className='padding-left-5'>
+                                                    <input
+                                                        type="text"
+                                                        placeholder="Name"
+                                                        value={name}
+                                                        onChange={updateName}
+                                                        className='inputs' />
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div className='padding-left-5' >Description</div>
+                                            <div className='
+                                border-bottom'>
+                                                <div className='padding-left-5'>
+                                                    <input
+                                                        type="text"
+                                                        placeholder="Description"
+                                                        value={description}
+                                                        onChange={updateDescription}
+                                                        className='inputs' />
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div className='padding-left-5' >Price</div>
+                                            <div className='
+                                height-30'>
+                                                <div className='padding-left-5'>
+                                                    <input
+                                                        type="number"
+                                                        placeholder="Price (numbers only!)"
+                                                        value={price}
+                                                        onChange={updatePrice}
+                                                        className='inputs' />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='
+                                flex-row
+                                width-90'>
+                                    <button type="submit" className='
+                                    looks-good-btn'>
+                                        Looks Good
+                                    </button>
+                                </div>
+
+                            </div>
+                        </form>
                     </div>
-                    <div>
-                        City
-                        <input
-                            type="text"
-                            placeholder="City"
-                            required
-                            value={city}
-                            onChange={updateCity} />
-                    </div>
-                    <div>
-                        State
-                        <input
-                            type="text"
-                            placeholder="State"
-                            required
-                            value={state}
-                            onChange={updateState} />
-                    </div>
-                    <div>
-                        Country:
-                        <input
-                            type="text"
-                            placeholder="Country"
-                            value={country}
-                            onChange={updateCountry} />
-                    </div>
-                    <div>
-                        Latitude:
-                        <input
-                            type="number"
-                            placeholder="Latitude (numbers only!)"
-                            value={lat}
-                            onChange={updateLat} />
-                    </div>
-                    <div>
-                        Longitude:
-                        <input
-                            type="number"
-                            placeholder="Longitude (numbers only!)"
-                            value={lng}
-                            onChange={updateLng} />
-                    </div>
-                    <div>
-                        Name:
-                        <input
-                            type="text"
-                            placeholder="Name"
-                            value={name}
-                            onChange={updateName} />
-                    </div>
-                    <div>
-                        Description:
-                        <input
-                            type="text"
-                            placeholder="Description"
-                            value={description}
-                            onChange={updateDescription} />
-                    </div>
-                    <div>
-                        Price:
-                        <input
-                            type="number"
-                            placeholder="Price (numbers only!)"
-                            value={price}
-                            onChange={updatePrice} />
-                    </div>
-                    <button type="submit">Edit this Spot</button>
-                </form>
+                </div>
             </div>
-        </section>
+        </div>
+
     );
 };
 
