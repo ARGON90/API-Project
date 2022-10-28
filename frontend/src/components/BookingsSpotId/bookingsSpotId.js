4a6d0ed41c7c58b37310bee81775798aa0cb4453
argon <argonglach@gmail.com>
2022-10-28 01:19:40 -0400

    got the dates to highlight for each month, renders dynamically!

--- frontend/src/components/BookingsSpotId/bookingsSpotId.js
+++ frontend/src/components/BookingsSpotId/bookingsSpotId.js
@@ -15,6 +15,7 @@ const BookingsSpotId = ({ rating, price, id }) => {
     const userBookings = useSelector((state) => (state?.bookings?.Bookings));
     const spotBookings = useSelector((state) => (state?.spotBookings?.Bookings));
 
+    const [calClick, setCalClick] = useState(false)
     const [showCalendar, setShowCalendar] = useState(false)
     const [cleanUp, setCleanUp] = useState(false)
     const [checkIn, setCheckIn] = useState('');
@@ -35,7 +36,7 @@ const BookingsSpotId = ({ rating, price, id }) => {
         dispatch(getBookingsCurrentSpot(id))
         calendarDates()
         return setCleanUp(true)
-    }, [dispatch, checkIn, checkOut, showCalendar])
+    }, [dispatch, checkIn, checkOut, showCalendar, calClick])
 
     if (!currentUserId) return <div>Log in to create a booking!</div>
     if (!userBookings) return <div>Log in to create a booking!</div>
@@ -45,120 +46,172 @@ const BookingsSpotId = ({ rating, price, id }) => {
     const userBookingsSpot = userBookingsArray.filter(booking => booking?.spotId === Number(id))
     spotBookingsArray = Object.values(spotBookings)
 
+    function calClicker() {
+        setCalClick(!calClick)
+        calendarDates()
+    }
+    console.log(calClick, 'cal click')
 
     function calendarDates() {
         if (!spotBookingsArray) {
             return null
         }
         let classStyler = spotBookingsArray.map((el) => {
-            let dateArr = (dateParser(el.startDate))
+            let startDateArr = (dateParser(el.startDate))
+            let endDateArr = (dateParser(el.endDate))
+            let startDate = el.startDate
+            let startDay = startDateArr[2]
+            let endDay = endDateArr[2]
+            let totalDays = Number(endDay) - Number(startDay)
 
-            let year = dateArr[0]
-            let month = dateArr[1]
-            let day = dateArr[2]
-            let dateString = `${month} ${day}, ${year}`
-            let queryInput = `[aria-label="${dateString}"]`
-            let element = document?.querySelector(queryInput)
-            if (element) {
-                element.className = 'pink'
+            for (let i = 0; i <= totalDays; i++) {
+                let startDateMarker =new Date(startDate)
+                startDateMarker.setTime(startDateMarker.getTime() + (86400000 * (i + 1)))
+                // console.log(startDateMarker)
+                let dateStr = String(startDateMarker)
+                let dateStrArray = dateStr.split(' ')
+                let day = dayParser(dateStrArray[2])
+                console.log(dayParser(day))
+                let month = monthParser(dateStrArray[1])
+                let year = dateStrArray[3]
+                let dateString = `${month} ${day}, ${year}`
+                let dateQueryInput = `[aria-label="${dateString}"]`
+                let endElement = document?.querySelector(dateQueryInput)
+                console.log(endElement)
+                console.log(dateString)
+                if (endElement) {
+                    endElement.className = 'pink'
+                }
             }
         })
+}
+
+console.log('November 01, 2022' === 'November 1, 2022')
+
+function monthParser(month) {
+    if (month === 'Oct') return "October"
+    if (month === 'Nov') return "November"
+    if (month === 'Dec') return "December"
+    if (month === 'Jan') return "January"
+    if (month === 'Feb') return "February"
+    if (month === 'Mar') return "March"
+    if (month === 'Apr') return "April"
+    if (month === 'May') return "May"
+    if (month === 'Jun') return "June"
+    if (month === 'Jul') return "July"
+    if (month === 'Aug') return "August"
+    if (month === 'Sep') return "September"
+}
+
+function dayParser(day) {
+    if (day === '01') return '1'
+    if (day === '02') return '2'
+    if (day === '03') return '3'
+    if (day === '04') return '4'
+    if (day === '05') return '5'
+    if (day === '06') return '6'
+    if (day === '07') return '7'
+    if (day === '08') return '8'
+    if (day === '09') return '9'
+
+    else {
+        return day
+    }
+}
+
+function dateParser(string) {
+    string = String(string)
+    let dateArray = string.split('-')
+    let dayPre = dateArray[2].split(' ')
+
+    const months = {
+        1: 'January',
+        2: 'February',
+        3: 'March',
+        4: 'April',
+        5: 'May',
+        6: 'June',
+        7: 'July',
+        8: 'August',
+        9: 'September',
+        10: 'October',
+        11: 'November',
+        12: 'December',
+    }
+    let year = dateArray[0]
+    let month = months[dateArray[1]]
+    let day = dayPre[0]
+    return [year, month, day]
+}
+
+const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    let checkInDateUser = new Date(checkIn)
+    let checkOutDateUser = new Date(checkOut)
+    let bookingErrors = {}
+
+    let today = new Date()
+    for (let i = 0; i < spotBookingsArray.length; i++) {
+        let existingStartDate = new Date(spotBookingsArray[i].startDate)
+        let existingEndDate = new Date(spotBookingsArray[i].endDate)
+
+        let existingStartDateErrors = String(existingStartDate).split(' ')
+        let monthStartDateError = existingStartDateErrors[1]
+        let dayStartDateError = existingStartDateErrors[2]
+        let yearStartDateError = existingStartDateErrors[3]
+
+        let existingEndDateErrors = String(existingEndDate).split(' ')
+        let monthEndDateError = existingEndDateErrors[1]
+        let dayEndDateError = existingEndDateErrors[2]
+        let yearEndDateError = existingEndDateErrors[3]
+
+        if (!checkIn) bookingErrors.checkIn = 'Select a check-in date'
+        if (!checkOut) bookingErrors.checkOut = 'Select a check-out date'
+        // start is in the past
+        if (checkInDateUser.getTime() < today.getTime()) bookingErrors.past = ('You cannot select a date in the past')
+        // end is before start
+        if (checkInDateUser.getTime() > checkOutDateUser.getTime()) bookingErrors.future = ('Check-out must be after check-in')
+        // start date inside of an existing booking
+        if (checkInDateUser.getTime() >= existingStartDate.getTime() &&
+            checkInDateUser.getTime() <= existingEndDate.getTime()) {
+            bookingErrors.startConflict = ("This start date conflicts with an existing booking")
+        }
+        // end date inside of existing booking
+        if (checkOutDateUser.getTime() >= existingStartDate.getTime() &&
+            checkOutDateUser.getTime() <= existingEndDate.getTime()) {
+            bookingErrors.endConflict = ("This end date conflicts with an existing booking")
+        }
+        // check in /checkout 'surround' existing booking
+        if (checkInDateUser.getTime() <= existingStartDate.getTime() &&
+            checkOutDateUser.getTime() >= existingEndDate.getTime()) {
+            bookingErrors.conflict = (`These dates overlap with an existing booking`)
+            //bookingErrors.conflict = (`Date Conflict: this property is booked between ${monthStartDateError} ${dayStartDateError} ${yearStartDateError}
+            // and ${monthEndDateError} ${dayEndDateError} ${yearEndDateError}. Please try different dates`)
+        }
+
+        setErrors((errors) => errors = Object.values(bookingErrors))
     }
 
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        let checkInDateUser = new Date(checkIn)
-        let checkOutDateUser = new Date(checkOut)
-        let bookingErrors = {}
-
-        let today = new Date()
-        for (let i = 0; i < spotBookingsArray.length; i++) {
-            let existingStartDate = new Date(spotBookingsArray[i].startDate)
-            let existingEndDate = new Date(spotBookingsArray[i].endDate)
-
-            let existingStartDateErrors = String(existingStartDate).split(' ')
-            let monthStartDateError = existingStartDateErrors[1]
-            let dayStartDateError = existingStartDateErrors[2]
-            let yearStartDateError = existingStartDateErrors[3]
-
-            let existingEndDateErrors = String(existingEndDate).split(' ')
-            let monthEndDateError = existingEndDateErrors[1]
-            let dayEndDateError = existingEndDateErrors[2]
-            let yearEndDateError = existingEndDateErrors[3]
-
-            if (!checkIn) bookingErrors.checkIn = 'Select a check-in date'
-            if (!checkOut) bookingErrors.checkOut = 'Select a check-out date'
-            // start is in the past
-            if (checkInDateUser.getTime() < today.getTime()) bookingErrors.past = ('You cannot select a date in the past')
-            // end is before start
-            if (checkInDateUser.getTime() > checkOutDateUser.getTime()) bookingErrors.future = ('Check-out must be after check-in')
-            // start date inside of an existing booking
-            if (checkInDateUser.getTime() >= existingStartDate.getTime() &&
-                checkInDateUser.getTime() <= existingEndDate.getTime()) {
-                bookingErrors.startConflict = ("This start date conflicts with an existing booking")
-            }
-            // end date inside of existing booking
-            if (checkOutDateUser.getTime() >= existingStartDate.getTime() &&
-                checkOutDateUser.getTime() <= existingEndDate.getTime()) {
-                bookingErrors.endConflict = ("This end date conflicts with an existing booking")
-            }
-            // check in /checkout 'surround' existing booking
-            if (checkInDateUser.getTime() <= existingStartDate.getTime() &&
-                checkOutDateUser.getTime() >= existingEndDate.getTime()) {
-                bookingErrors.conflict = (`These dates overlap with an existing booking`)
-                //bookingErrors.conflict = (`Date Conflict: this property is booked between ${monthStartDateError} ${dayStartDateError} ${yearStartDateError}
-                // and ${monthEndDateError} ${dayEndDateError} ${yearEndDateError}. Please try different dates`)
-            }
-
-            setErrors((errors) => errors = Object.values(bookingErrors))
-        }
-
-        const bookingInfo = {
-            spotId: id,
-            userId: currentUserId,
-            startDate: checkIn,
-            endDate: checkOut
-        }
-
-        if (errors.length === 0) {
-            let createdBooking;
-            createdBooking = await dispatch()
-        }
+    const bookingInfo = {
+        spotId: id,
+        userId: currentUserId,
+        startDate: checkIn,
+        endDate: checkOut
     }
 
-    function dateParser(string) {
-        string = String(string)
-        let dateArray = string.split('-')
-        let dayPre = dateArray[2].split(' ')
-
-        const months = {
-            1: 'January',
-            2: 'February',
-            3: 'March',
-            4: 'April',
-            5: 'May',
-            6: 'June',
-            7: 'July',
-            8: 'August',
-            9: 'September',
-            10: 'October',
-            11: 'November',
-            12: 'December',
-        }
-
-        let year = dateArray[0]
-        let month = months[dateArray[1]]
-        let day = dayPre[0]
-        return [year, month, day]
+    if (errors.length === 0) {
+        let createdBooking;
+        createdBooking = await dispatch()
     }
+}
 
-    if (!userBookingsArray) return <div>Loading Bookings</div>
-    return (
-        <>
-            <div>
-                {/* {userBookingsSpot.length > 0 &&
+
+if (!userBookingsArray) return <div>Loading Bookings</div>
+return (
+    <>
+        <div>
+            {/* {userBookingsSpot.length > 0 &&
                     <div className='user-booking-container'>
                         <div className='booking-title'> Your Bookings </div>
                         {userBookingsSpot.map((booking) =>
@@ -171,86 +224,86 @@ const BookingsSpotId = ({ rating, price, id }) => {
                     </div>
                 } */}
 
-                {errors.length > 0 &&
-                    <div className='errors-container'>
-                        <div className='errors-title'> Sorry, there's a problem with your selection:</div>
-                        <ul>
-                            {errors.map((error, idx) =>
-                                <div key={idx}>
-                                    <li>{error}</li>
-                                </div>
-                            )}
-                        </ul>
-                    </div>
-                }
-
-
-                {currentUserId &&
-                    <form onSubmit={handleSubmit} className='booking-container'>
-                        <div className='booking-title'>Create a Booking</div>
-
-                        <div className='price-stars'>
-                            <div>${price} dollars per night</div>
-                            <div>
-                                <svg viewBox='0 0 32 32'>
-                                    <path
-                                        d='M15.094 1.579l-4.124 8.885-9.86 1.27a1 1 0 0 0-.542 1.736l7.293 6.565-1.965 9.852a1 1 0 0 0 1.483 1.061L16 25.951l8.625 4.997a1 1 0 0 0 1.482-1.06l-1.965-9.853 7.293-6.565a1 1 0 0 0-.541-1.735l-9.86-1.271-4.127-8.885a1 1 0 0 0-1.814 0z'
-                                        fillRule='evenodd'
-                                    ></path>
-                                </svg>
-                                {rating}
-                            </div>
-                        </div>
-
-                        <div className='check-dates-container'>
-                            <div className='form-container'>
-                                <div className='date-container'>
-                                    <label>Check-in</label>
-                                    <input type='date' className='date-input'
-                                        value={checkIn} onChange={updateCheckIn}>
-                                    </input>
-                                </div>
-
-                                <div className='date-container'>
-                                    <label>Check-Out</label>
-                                    <input type='date' className='date-input'
-                                        value={checkOut} onChange={updateCheckOut}>
-                                    </input>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className='subtotal'>
-                            <div>dollars</div>
-                            <div>nights</div>
-                        </div>
-                        <div className='total'>Total $</div>
-                        <button type='submit' className='reserve'>Reserve</button>
-                    </form>
-                }
-                {spotBookingsArray.length > 0 &&
-                    <div className='spot-booking-container'>
-                        <div className='booking-title'> This Spot's Bookings </div>
-                        {spotBookingsArray.map((booking) =>
-                            <div key={booking.id}>
-                                <div>{`${dateParser(booking.startDate)[1]} ${dateParser(booking.startDate)[2]}, ${dateParser(booking.startDate)[0]} -
-                        ${dateParser(booking.endDate)[1]} ${dateParser(booking.endDate)[2]}, ${dateParser(booking.endDate)[0]}`}
-                                </div>
+            {errors.length > 0 &&
+                <div className='errors-container'>
+                    <div className='errors-title'> Sorry, there's a problem with your selection:</div>
+                    <ul>
+                        {errors.map((error, idx) =>
+                            <div key={idx}>
+                                <li>{error}</li>
                             </div>
                         )}
-                    </div>
-                }
+                    </ul>
+                </div>
+            }
 
-                <button onClick={() => setShowCalendar(!showCalendar)}>show cal</button>
-                {showCalendar &&
-                    <div className='react-calendar'>
-                        <Calendar />
+
+            {currentUserId &&
+                <form onSubmit={handleSubmit} className='booking-container'>
+                    <div className='booking-title'>Create a Booking</div>
+
+                    <div className='price-stars'>
+                        <div>${price} dollars per night</div>
+                        <div>
+                            <svg viewBox='0 0 32 32'>
+                                <path
+                                    d='M15.094 1.579l-4.124 8.885-9.86 1.27a1 1 0 0 0-.542 1.736l7.293 6.565-1.965 9.852a1 1 0 0 0 1.483 1.061L16 25.951l8.625 4.997a1 1 0 0 0 1.482-1.06l-1.965-9.853 7.293-6.565a1 1 0 0 0-.541-1.735l-9.86-1.271-4.127-8.885a1 1 0 0 0-1.814 0z'
+                                    fillRule='evenodd'
+                                ></path>
+                            </svg>
+                            {rating}
+                        </div>
                     </div>
-                }
-                {calendarDates()}
-            </div>
-        </>
-    );
+
+                    <div className='check-dates-container'>
+                        <div className='form-container'>
+                            <div className='date-container'>
+                                <label>Check-in</label>
+                                <input type='date' className='date-input'
+                                    value={checkIn} onChange={updateCheckIn}>
+                                </input>
+                            </div>
+
+                            <div className='date-container'>
+                                <label>Check-Out</label>
+                                <input type='date' className='date-input'
+                                    value={checkOut} onChange={updateCheckOut}>
+                                </input>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='subtotal'>
+                        <div>dollars</div>
+                        <div>nights</div>
+                    </div>
+                    <div className='total'>Total $</div>
+                    <button type='submit' className='reserve'>Reserve</button>
+                </form>
+            }
+            {spotBookingsArray.length > 0 &&
+                <div className='spot-booking-container'>
+                    <div className='booking-title'> This Spot's Bookings </div>
+                    {spotBookingsArray.map((booking) =>
+                        <div key={booking.id}>
+                            <div>{`${dateParser(booking.startDate)[1]} ${dateParser(booking.startDate)[2]}, ${dateParser(booking.startDate)[0]} -
+                        ${dateParser(booking.endDate)[1]} ${dateParser(booking.endDate)[2]}, ${dateParser(booking.endDate)[0]}`}
+                            </div>
+                        </div>
+                    )}
+                </div>
+            }
+
+            <button onClick={() => setShowCalendar(!showCalendar)}>See this Spot's Bookings</button>
+            {showCalendar &&
+                <div className='react-calendar' onClick={() => calClicker()}>
+                    <Calendar />
+                </div>
+            }
+            {calendarDates()}
+        </div>
+    </>
+);
 };
 
 export default BookingsSpotId;
